d7ad8732a0b235ba57482e890cbac298ab82d24d
gennsan0108 <youta.1818.f@gmail.com>
2026-04-20 21:59:39 +0900

    displays Name and Icon

--- Assets/Scripts/PersonManagement.cs
+++ Assets/Scripts/PersonManagement.cs
@@ -1,6 +1,9 @@
+using Microsoft.Unity.VisualStudio.Editor;
+
 public class PersonManagement
 {
     public string playerName;
+    public Image playerIcon;
     public PersonManagement(string playerName)
     {
         this.playerName = playerName;
